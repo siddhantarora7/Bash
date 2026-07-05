d81342059331a16c48e2e982b63c4ea28688f05d
Siddhant Arora <siddaroraleo@gmail.com>
2026-07-04 22:58:44 -0600

    Initialize app boot text

--- local_game.py
+++ local_game.py
@@ -1,6 +1,26 @@
+from rich.console import Console
+from rich.text import Text
+console = Console()
+
 from questions import load
 from questions import check
 import random
+
+rainbow = ["red", "orange", "yellow", "green", "cyan", "blue", "indigo", "magenta"]
+
+t = Text()
+art = r'''    ____  ___   _____ __  __
+   / __ )/   | / ___// / / /
+  / __  / /| | \__ \/ /_/ / 
+ / /_/ / ___ |___/ / __  /  
+/_____/_/  |_/____/_/ /_/   '''
+
+for line in art.splitlines():
+    for x, ch in enumerate(line):
+        t.append(ch, style = rainbow[x % len(rainbow)])
+    t.append("\n")
+
+console.print(t)
 
 score = 0
 
