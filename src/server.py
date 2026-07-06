ffd06d75446d2f28fd436a9a23fc8fe455a80398
Siddhant Arora <siddaroraleo@gmail.com>
2026-07-06 17:28:47 -0600

    Make handle() only serve queue input and player entry

--- src/server.py
+++ src/server.py
@@ -16,29 +16,17 @@ QUESTIONS = load()
 async def handle(reader, writer):
     join = await read_msg(reader)
     name = join["name"]
-    
-    score = 0
-    inds = list(range(len(QUESTIONS)))
-    for i in range(min(5, len(QUESTIONS))):
-        idx = random.choice(inds)
-        inds.remove(idx)
-        q = QUESTIONS[idx]
+    room.players[name] = writer
+    room.scores[name] = 0
+    await room.queue.put(("join", name))
 
-        await send_msg(writer, {"type": "question", "num": i + 1, "text": q.question})
-        reply = await read_msg(reader)
-
-        if reply is None:
+    while True:
+        msg = await read_msg(reader)
+        if msg is None:
+            await room.queue.put(("leave", name))
             break
-
-        if check(reply["answer"], q.answer):
-            score += 1
-            await send_msg(writer, {"type": "result", "verdict": "correct", "answer": q.answer})
-        else:
-            await send_msg(writer, {"type": "result", "verdict": "wrong", "answer": q.answer})
-    
-    await send_msg(writer, {"type": "game_over", "final_score": score})
-    writer.close()
-    await writer.wait_closed()
+        if msg["type"] == "submit":
+            await room.queue.put(("submit", name, msg["answer"]))
 
 # Actually start server
 async def main():
